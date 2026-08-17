23aa56a43d2b2b7aa3bfd65bfb43d2c9bd5739b2
Daric Hughes <38288575+DaricHughesJ@users.noreply.github.com>
2026-08-17 11:05:34 -0500

    Route legal pages

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,13 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import LegalDocs from './components/LegalDocs.jsx'
+
+const path = window.location.pathname.replace(/\/+$/, '') || '/'
+const content = path === '/privacy' ? <LegalDocs type="privacy"/> : path === '/terms' ? <LegalDocs type="terms"/> : <App/>
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    {content}
   </StrictMode>,
 )
